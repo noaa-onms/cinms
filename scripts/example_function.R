f5b5edc58787c0a7b5ddaf20abd09de5ec7c0d26
superjai <jai.ranganathan@gmail.com>
2020-06-15 11:09:23 -0700

    Slight change

--- scripts/example_function.R
+++ scripts/example_function.R
@@ -4,6 +4,6 @@ example_function <- function(modal_window){
   modal_path = paste0(here::here("modals"),"/")
   
   # create the intermediary markdown file
-  render(paste0(modal_path, modal_window, ".Rmd"), output_dir = modal_path, output_format = "md_document", clean = F)
+  rmarkdown::render(paste0(modal_path, modal_window, ".Rmd"), output_dir = modal_path, output_format = "md_document", clean = F)
 
 }
