1e9c4afb26928c83c5f53eeb4108dd7d0efa401e
superjai <jai.ranganathan@gmail.com>
2020-07-26 21:28:04 -0700

    Update test_script2.r

--- scripts/test_script2.r
+++ scripts/test_script2.r
@@ -1,2 +1,2 @@
 output_test = paste(as.character(Sys.time()))
-write(output_test, file = "../data/oceano/deletethis.txt", append = T)
+write(output_test, file = "data/oceano/deletethis.txt", append = T)
