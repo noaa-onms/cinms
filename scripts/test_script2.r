291ed875b8654c75caa20bcc2536392d9d1b990a
superjai <jai.ranganathan@gmail.com>
2020-08-28 08:03:20 -0700

    testing workflow

--- scripts/test_script2.r
+++ scripts/test_script2.r
@@ -1,4 +1,2 @@
 output_test = paste(as.character(Sys.time()))
 write(output_test, file = "data/oceano/deletethis.txt", append = T)
-#devtools::install_github("marinebon/nms4r")
-#nms4r::generate_latest_SST("cinms","jplMURSST41mday", "sst", c("mean", "sd"))
