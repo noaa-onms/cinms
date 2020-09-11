31eaedd6b7114dde5e2102e95186b4b264921a95
Ben Best <ben@ecoquants.com>
2020-09-11 11:42:34 -0700

    comparing w/ nc_sst w/ @superjai

--- scripts/test_script_sept11.R
+++ scripts/test_script_sept11.R
@@ -49,6 +49,19 @@ month = 7
   bb <- st_bbox(sanctuary_ply)
   
   # pull the raster data
+  
+  m_dates_sst <- c(m_beg, ymd(glue("{year}-{month+1}-01")))
+  
+  m_beg_sst   <- ymd(glue("{year}-{month}-01"))
+  m_end_sst   <- m_beg + days(days_in_month(m_beg)) - days(1)
+  m_dates_sst <- c(m_beg_sst, m_end_sst)
+  
+  nc_sst <- griddap(
+    info("jplMURSST41mday"), 
+    time = m_dates_sst, 
+    latitude = c(bb$ymin, bb$ymax), longitude = c(bb$xmax, bb$xmin), 
+    fields = "sst", fmt = 'nc')
+  
   nc <- griddap(
     info(erddap_id), 
     time = m_dates, 
@@ -60,8 +73,14 @@ month = 7
   # [1] "var (or dimvar) name: coord_ref"
   # [1] "file name: /Users/jai/Library/Caches/R/rerddap/e93f320f7d59040b4824d520e998c936.nc"
   
-  r <- raster(nc$summary$filename)
+  
+  r <- raster(nc_sst$summary$filename)
+  
+  r <- raster(nc$summary$filename, varname = erddap_fld)
+  
+  
 
+  nc$summary$var[[erddap_fld]]$longname
   # but the raster exists!
   plot(raster(nc$summary$filename))
   
