1ca6b7b716216c4447521d4555342f4978ab3afe
Ben Best <ben@ecoquants.com>
2020-09-24 13:48:21 -0700

    rmd2html() to render(): glossary off b/c misses dynamic plots

--- scripts/render_oceano-modals.R
+++ scripts/render_oceano-modals.R
@@ -5,8 +5,10 @@ print('Rendering oceano_Rmds')
 # get the one working version for now 
 oceano_Rmds <- list.files(here("modals"), "^key-climate-ocean.Rmd$", full.names = T) 
 # TODO: get other Rmds above to work:
-#oceano_Rmds <- list.files(here("modals"), "^[^_].*climate-ocean.Rmd$", full.names = T)
+#   oceano_Rmds <- list.files(here("modals"), "^[^_].*climate-ocean.Rmd$", full.names = T)
 
-walk(oceano_Rmds, rmd2html)
+walk(oceano_Rmds, render)
+# TODO: fix rmd2html so transfers all JavaScript libraries over from md to html
+#   walk(oceano_Rmds, rmd2html)
 
 print ('Finished rendering oceano_Rmds')
