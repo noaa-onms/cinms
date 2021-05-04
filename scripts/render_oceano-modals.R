05a7c63be38d84e304ee32b06a5d2d7b97bbb62b
Ben Best <ben@ecoquants.com>
2021-05-04 16:43:59 -0700

    DEBUG GH Action for kelp-forest_key-climate-ocean.Rmd #79

--- scripts/render_oceano-modals.R
+++ scripts/render_oceano-modals.R
@@ -1,5 +1,7 @@
 library(nms4r)
 
-print('Rendering oceano_Rmds')
-nms4r::render_all_rmd(nms = "cinms", interactive_only = T)
+#print('Rendering oceano_Rmds')
+#nms4r::render_all_rmd(nms = "cinms", interactive_only = T)
+print('Rendering modals/kelp-forest_key-climate-ocean.Rmd')
+nms4r::generate_html_4_rmd(here::here("modals/kelp-forest_key-climate-ocean.Rmd"))
 print ('Finished rendering oceano_Rmds')
